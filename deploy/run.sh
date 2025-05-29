c4778c92def704894eb02daf163acff26e3d5b2b
ddem-gast <dann.demetre@gastronomous.ca>
2025-05-28 21:38:31 -0400

    Update run.sh

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash 
+
+#See section 1.2.1
+#https://www.st.com/resource/en/user_manual/um2237-stm32cubeprogrammer-software-description-stmicroelectronics.pdf
 docker run -it -v /dev:/dev --device /dev:/dev --net=host -it blower_app
