b82bbcc5ee6e1cb65ec91243666ca8174ea09657
ddem-gast <dann.demetre@gastronomous.ca>
2025-05-29 22:19:23 -0400

    Update run.sh

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR/../
-
 #See section 1.2.1
 #https://www.st.com/resource/en/user_manual/um2237-stm32cubeprogrammer-software-description-stmicroelectronics.pdf
 docker run --rm -it -v /dev:/dev --device /dev:/dev --net=host blower_app
