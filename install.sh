66f0635b6c3583be68571b4266ca70bf1974163c
Dann Demetre <dann.demetre@gastronomous.ca>
2025-05-31 11:35:54 -0400

    adding in apt-package to ensure rootless is present

--- install.sh
+++ install.sh
@@ -12,7 +12,23 @@ FD_RULES=$(ls *stlinkv2*.rules)
 function inst_docker {
   #Docker Installation
   if command -v docker; then
+
+    # Allowing packages to be found to ensure rootless installation
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
     sudo apt-get install docker-ce-rootless-extras -y 
+
   else
     echo "Installing Docker"
     curl -sSL https://get.docker.com | sh
