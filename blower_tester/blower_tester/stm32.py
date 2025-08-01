c98ef9c822fa80183c289568c34a1c5ce2b4bed9
Dann Demetre <dann.demetre@gastronomous.ca>
2025-08-01 17:08:13 -0400

    A few adjustments, nothing major

--- blower_tester/blower_tester/stm32.py
+++ blower_tester/blower_tester/stm32.py
@@ -2,8 +2,8 @@ from .config import conf
 from .thermal_monitor import ThermalMonitor
 from time import time
 
-def __fw_version_valid(packet):
-    return packet.fw_version_major > 0
+def __fw_version_valid():
+    return ThermalMonitor.packet.fw_version_major > 0
 
 def do_spi_ack():
     TIMEOUT_S = 10
@@ -21,14 +21,13 @@ def get_tc_temp(tc_ch):
         conf["log"].exception("Error, desired thermocouple number must be 1 or 2")
         raise ValueError
     else:
-        for i in range(5):
-            ThermalMonitor.request_packet()
-            conf["log"].debug(f"Packet Received: \n{ThermalMonitor.packet}")
+        ThermalMonitor.request_packet()
+        conf["log"].debug(f"Packet Received: \n{ThermalMonitor.packet}")
         tc_temp = ThermalMonitor.packet.temp_sensor_C[tc_ch-1]
     return tc_temp
 
 def __fan_speed_valid(fan_index):
-    return ThermalMonitor.fan_speed_rpm[fan_index] > 0
+    return ThermalMonitor.packet.fan_speed_rpm[fan_index] > 0
 
 def get_fan_speed(fan_num):
     fan_speed = 0
@@ -36,14 +35,10 @@ def get_fan_speed(fan_num):
         conf["log"].exception("Error, desired fan number must be 1,2,3")
         raise ValueError
     else:
-        TIMEOUT_S = 10
-        start = time()
-        while not (__fan_speed_valid( fan_num-1) or (time() - start) > TIMEOUT_S):
-            ThermalMonitor.request_packet()
-            packet = ThermalMonitor.packet
-            if __fan_speed_valid(packet, fan_num-1):
-                conf["log"].debug(f"Packet Received: \n{ThermalMonitor.packet}")
-                fan_speed = ThermalMonitor.packet.fan_speed_rpm[fan_num-1]
-                break
+        ThermalMonitor.request_packet()
+        conf["log"].debug(f"Packet Received: \n{ThermalMonitor.packet}")
+        fan_speed = ThermalMonitor.packet.fan_speed_rpm[fan_num - 1]
+
     conf["log"].debug("Measured fan {:d}, speed: {:d} RPM".format(fan_num, fan_speed))
+
     return fan_speed
