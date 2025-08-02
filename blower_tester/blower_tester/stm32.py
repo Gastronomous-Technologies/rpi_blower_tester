69af562f4cc41b618ad5c5479d36782e62c9106e
Dann Demetre <dann.demetre@gastronomous.ca>
2025-08-01 22:34:45 -0400

    Nitpick

--- blower_tester/blower_tester/stm32.py
+++ blower_tester/blower_tester/stm32.py
@@ -1,20 +1,13 @@
 from .config import conf
 from .thermal_monitor import ThermalMonitor
-from time import time
 
 def __fw_version_valid():
     return ThermalMonitor.packet.fw_version_major > 0
 
 def do_spi_ack():
-    TIMEOUT_S = 10
-    start = time()
-    while not (__fw_version_valid() or (time() - start) > TIMEOUT_S):
-        ThermalMonitor.request_packet()
-        if __fw_version_valid():
-            conf["log"].debug(f"Packet Received: \n{ThermalMonitor.packet}")
-            return None
+    ThermalMonitor.request_packet()
     conf["log"].debug(f"Packet Received: \n{ThermalMonitor.packet}")
-    return "Could not communicate over SPI"
+    return None if __fw_version_valid() else "Could not communicate over SPI"
 
 def get_tc_temp(tc_ch):
     if tc_ch not in conf["tc"]["range"]:
@@ -25,9 +18,6 @@ def get_tc_temp(tc_ch):
         conf["log"].debug(f"Packet Received: \n{ThermalMonitor.packet}")
         tc_temp = ThermalMonitor.packet.temp_sensor_C[tc_ch-1]
     return tc_temp
-
-def __fan_speed_valid(fan_index):
-    return ThermalMonitor.packet.fan_speed_rpm[fan_index] > 0
 
 def get_fan_speed(fan_num):
     fan_speed = 0
